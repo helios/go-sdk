147e281b3790014cb6e4775b180a41117314dc1d
Lior Govrin <lior@heliosphere.io>
2022-11-29 13:51:52 +0200

    [HLS-2907] Mongo Proxy library

---
+++ proxy-libs/heliosmongo/go.mod
@@ -0,0 +1,33 @@
+module github.com/helios/go-sdk/proxy-libs/heliosmongo
+
+go 1.18
+
+require (
+	github.com/helios/helios-go-instrumenter/exports_extractor v0.0.1
+	github.com/stretchr/testify v1.8.1
+	go.mongodb.org/mongo-driver v1.11.0
+	go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo v0.36.4
+	go.opentelemetry.io/otel v1.11.1
+	go.opentelemetry.io/otel/sdk v1.11.1
+	go.opentelemetry.io/otel/trace v1.11.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/klauspost/compress v1.15.12 // indirect
+	github.com/montanaflynn/stats v0.6.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ proxy-libs/heliosmongo/mongo_test.go
@@ -0,0 +1,128 @@
+package heliosmongo
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
+	"go.opentelemetry.io/otel/trace"
+	exportsExtractor "github.com/helios/helios-go-instrumenter/exports_extractor"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+)
+
+func getSpanRecorder() *tracetest.SpanRecorder {
+	spanRecorder := tracetest.NewSpanRecorder()
+	otel.SetTracerProvider(sdktrace.NewTracerProvider(sdktrace.WithSpanProcessor(spanRecorder)))
+	propagator := propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{})
+	otel.SetTextMapPropagator(propagator)
+	return spanRecorder
+}
+
+func getClientOptions() *options.ClientOptions {
+	clientOptions := options.Client()
+	clientOptions.ApplyURI("mongodb://localhost:27017")
+	return clientOptions
+}
+
+func insertUser(client *mongo.Client, id int, name string, role string) {
+	users := client.Database("test").Collection("users")
+	_, error := users.InsertOne(context.Background(), bson.D{
+		{Key: "id", Value: id},
+		{Key: "name", Value: name},
+		{Key: "role", Value: role},
+	})
+
+	if error != nil {
+		panic(error)
+	}
+}
+
+func assertSpan(t *testing.T, spanRecorder *tracetest.SpanRecorder) []attribute.KeyValue {
+	spanRecorder.ForceFlush(context.Background())
+	spans := spanRecorder.Ended()
+	assert.Equal(t, 1, len(spans))
+	span := spans[0]
+	assert.Equal(t, "users.insert", span.Name())
+	assert.Equal(t, trace.SpanKindClient, span.SpanKind())
+	return span.Attributes()
+}
+
+func assertAttributes(t *testing.T, attributes []attribute.KeyValue, id int, name string, role string) {
+	for _, attribute := range attributes {
+		key := attribute.Key
+		value := attribute.Value.AsString()
+
+		switch key {
+		case semconv.DBMongoDBCollectionKey:
+			assert.Equal(t, "users", value)
+		case semconv.DBNameKey:
+			assert.Equal(t, "testing", value)
+		case semconv.DBOperationKey:
+			assert.Equal(t, "insert", value)
+		case semconv.DBStatementKey:
+			assert.Contains(t, value, fmt.Sprintf("\"id\":%v", id))
+			assert.Contains(t, value, fmt.Sprintf("\"name\":\"%v\"", name))
+			assert.Contains(t, value, fmt.Sprintf("\"role\":\"%v\"", role))
+		case semconv.DBSystemKey:
+			assert.Equal(t, "mongodb", value)
+		}
+	}
+}
+
+func TestConnectInstrumentation(t *testing.T) {
+	spanRecorder := getSpanRecorder()
+	clientOptions := getClientOptions()
+	client, error := Connect(context.Background(), clientOptions)
+
+	if error != nil {
+		panic(error)
+	}
+
+	insertUser(client, 12345, "Lior Govrin", "Software Engineer")
+	attributes := assertSpan(t, spanRecorder)
+	assertAttributes(t, attributes, 12345, "Lior Govrin", "Software Engineer")
+}
+
+func TestNewClientInstrumentation(t *testing.T) {
+	spanRecorder := getSpanRecorder()
+	clientOptions := getClientOptions()
+	client, error := NewClient(clientOptions)
+
+	if error != nil {
+		panic(error)
+	}
+
+	client.Connect(context.Background())
+	insertUser(client, 67890, "Bob McClown", "Company Jester")
+	attributes := assertSpan(t, spanRecorder)
+	assertAttributes(t, attributes, 67890, "Bob McClown", "Company Jester")
+}
+
+func TestInterfaceMatch(t *testing.T) {
+	// Get original mongo exports.
+	originalRepository := exportsExtractor.CloneGitRepository("https://github.com/mongodb/mongo-go-driver", "v1.11.0")
+	originalExports := exportsExtractor.ExtractExports(filepath.Join(originalRepository, "/mongo"), "mongo")
+	os.RemoveAll(originalRepository)
+	sort.Strings(originalExports)
+
+	// Get Helios mongo exports.
+	srcDir, _ := filepath.Abs(".")
+	heliosExports := exportsExtractor.ExtractExports(srcDir, "heliosmongo")
+	sort.Strings(heliosExports)
+
+	// Compare.
+	assert.True(t, reflect.DeepEqual(originalExports, heliosExports))
+}
