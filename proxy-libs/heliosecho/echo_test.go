bedd440d3fa8f8085cf4dfca5f7a58349b3e811d
Ran Nozik <ran@gethelios.dev>
2022-12-06 09:12:57 +0200

    add echo

---
+++ proxy-libs/heliosecho/echo_test.go
@@ -0,0 +1,84 @@
+package heliosecho
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sort"
+	"testing"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/trace/tracetest"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+
+	exportsExtractor "github.com/helios/helios-go-instrumenter/exports_extractor"
+	"github.com/stretchr/testify/assert"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+func validateAttributes(attrs []attribute.KeyValue, t *testing.T) {
+	for _, value := range attrs {
+		key := value.Key
+		if key == semconv.HTTPMethodKey {
+			assert.Equal(t, "GET", value.Value.AsString())
+		} else if key == semconv.HTTPTargetKey {
+			assert.Equal(t, "/users/abcd1234", value.Value.AsString())
+		} else if key == semconv.HTTPStatusCodeKey {
+			assert.Equal(t, 200, int(value.Value.AsInt64()))
+		} else if key == semconv.HTTPRouteKey {
+			assert.Equal(t, "/users/:id", value.Value.AsString())
+		}
+	}
+}
+
+func TestInstrumentation(t *testing.T) {
+	sr := tracetest.NewSpanRecorder()
+	otel.SetTracerProvider(sdktrace.NewTracerProvider(sdktrace.WithSpanProcessor(sr)))
+	propagator := propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{})
+	otel.SetTextMapPropagator(propagator)
+	r := New()
+
+	r.GET("/users/:id", func(c Context) error {
+		id := c.Param("id")
+		name := "Random"
+		return c.JSON(http.StatusOK, struct {
+			ID   string
+			Name string
+		}{
+			ID:   id,
+			Name: name,
+		})
+	})
+
+	go func() {
+		_ = r.Start(":8091")
+	}()
+
+	http.Get("http://localhost:8091/users/abcd1234")
+	sr.ForceFlush(context.Background())
+	spans := sr.Ended()
+	assert.Equal(t, 1, len(spans))
+	serverSpan := spans[0]
+	validateAttributes(serverSpan.Attributes(), t)
+}
+
+func TestInterfaceMatch(t *testing.T) {
+	originalRepository := exportsExtractor.CloneGitRepository("https://github.com/labstack/echo", "v4.9.1")
+	originalExports := exportsExtractor.ExtractExports(originalRepository, "echo")
+	os.RemoveAll(originalRepository)
+	sort.Slice(originalExports, func(i, j int) bool {
+		return originalExports[i].Name < originalExports[j].Name
+	})
+
+	srcDir, _ := filepath.Abs(".")
+	heliosExports := exportsExtractor.ExtractExports(srcDir, "heliosecho")
+	sort.Slice(heliosExports, func(i, j int) bool {
+		return heliosExports[i].Name < heliosExports[j].Name
+	})
+
+	// Compare.
+	assert.EqualValues(t, originalExports, heliosExports)
+}
---
+++ proxy-libs/heliosecho/go.mod
@@ -0,0 +1,38 @@
+module github.com/helios/go-sdk/proxy-libs/heliosecho
+
+go 1.18
+
+require (
+	github.com/helios/helios-go-instrumenter/exports_extractor v0.0.3
+	github.com/labstack/echo/v4 v4.9.1
+	github.com/stretchr/testify v1.8.1
+	go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho v0.37.0
+	go.opentelemetry.io/otel v1.11.2
+	go.opentelemetry.io/otel/sdk v1.11.2
+)
+
+require (
+	github.com/kr/pretty v0.3.0 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/rogpeppe/go-internal v1.8.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	go.opentelemetry.io/otel/trace v1.11.2 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
