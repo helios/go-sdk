edecf3009c3096e61b945d306a4a0a5c3777f6a9
Lior Govrin <lior@heliosphere.io>
2022-12-07 17:28:24 +0200

    [HLS-2910] Add mux proxy lib

---
+++ proxy-libs/heliosmux/go.mod
@@ -0,0 +1,13 @@
+module github.com/helios/go-sdk/proxy-libs/heliosmux
+
+go 1.12
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/helios/helios-go-instrumenter/exports_extractor v0.0.5
+	github.com/stretchr/testify v1.8.1
+	go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux v0.37.0
+	go.opentelemetry.io/otel v1.11.2
+	go.opentelemetry.io/otel/sdk v1.11.2
+	go.opentelemetry.io/otel/trace v1.11.2
+)
---
+++ proxy-libs/heliosmux/mux.go
@@ -0,0 +1,42 @@
+package heliosmux
+
+import (
+	"net/http"
+
+	originalMux "github.com/gorilla/mux"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
+)
+
+type BuildVarsFunc = originalMux.BuildVarsFunc
+type MatcherFunc = originalMux.MatcherFunc
+type MiddlewareFunc = originalMux.MiddlewareFunc
+type Route = originalMux.Route
+type RouteMatch = originalMux.RouteMatch
+type Router = originalMux.Router
+type WalkFunc = originalMux.WalkFunc
+
+var ErrMethodMismatch = originalMux.ErrMethodMismatch
+var ErrNotFound = originalMux.ErrNotFound
+var SkipRouter = originalMux.SkipRouter
+
+func CORSMethodMiddleware(r *Router) MiddlewareFunc {
+	return originalMux.CORSMethodMiddleware(r)
+}
+
+func CurrentRoute(r *http.Request) *Route {
+	return originalMux.CurrentRoute(r)
+}
+
+func NewRouter() *Router {
+	router := originalMux.NewRouter()
+	router.Use(otelmux.Middleware("helios"))
+	return router
+}
+
+func SetURLVars(r *http.Request, val map[string]string) *http.Request {
+	return originalMux.SetURLVars(r, val)
+}
+
+func Vars(r *http.Request) map[string]string {
+	return originalMux.Vars(r)
+}
