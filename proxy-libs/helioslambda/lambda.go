6b3ad0cf40c110cadec2096131661dc567353add
Ran Nozik <ran@gethelios.dev>
2023-01-17 08:35:10 +0200

    fix tests

--- proxy-libs/helioslambda/lambda.go
+++ proxy-libs/helioslambda/lambda.go
@@ -13,7 +13,7 @@ func Start(handler interface{}) {
 	lambda.Start(otellambda.InstrumentHandler(handler))
 }
 
-func StartWithOptions(handler interface{}, options ...lambda.Option) {
+func StartWithOptions(handler interface{}, options ...Option) {
 	lambda.StartWithOptions(otellambda.InstrumentHandler(handler), options...)
 }
 
@@ -21,6 +21,42 @@ func StartWithContext(ctx context.Context, handler interface{}) {
 	lambda.StartWithContext(ctx, otellambda.InstrumentHandler(handler))
 }
 
-func StartHandler(handler lambda.Handler) {
+func StartHandlerWithContext(ctx context.Context, handler Handler) {
+	lambda.StartWithOptions(handler, WithContext(ctx))
+}
+
+func StartHandler(handler Handler) {
 	lambda.StartHandler(handler)
+}
+
+type Handler = lambda.Handler
+type Function = lambda.Function
+type Option = lambda.Option
+
+func WithContext(ctx context.Context) Option {
+	return lambda.WithContext(ctx)
+}
+
+func WithSetEscapeHTML(escapeHTML bool) Option {
+	return lambda.WithSetEscapeHTML(escapeHTML)
+}
+
+func WithSetIndent(prefix, indent string) Option {
+	return lambda.WithSetIndent(prefix, indent)
+}
+
+func WithEnableSIGTERM(callbacks ...func()) Option {
+	return lambda.WithEnableSIGTERM(callbacks...)
+}
+
+func NewHandler(handlerFunc interface{}) Handler {
+	return lambda.NewHandler(handlerFunc)
+}
+
+func NewHandlerWithOptions(handlerFunc interface{}, options ...Option) Handler {
+	return lambda.NewHandlerWithOptions(handlerFunc, options...)
+}
+
+func NewFunction(handler Handler) *Function {
+	return lambda.NewFunction(handler)
 }
