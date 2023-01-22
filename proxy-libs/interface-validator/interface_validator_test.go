3fd537789db6897aaaf805b659a59b2feba10d12
LironKS <liron@gethelios.dev>
2023-01-22 13:47:51 +0200

    add note about deleting methodrtyu

--- proxy-libs/interface-validator/interface_validator_test.go
+++ proxy-libs/interface-validator/interface_validator_test.go
@@ -73,7 +73,7 @@ func TestEchoInterfaceMatch(t *testing.T) {
 func TestS3InterfaceMatch(t *testing.T) {
 	originalExports := cloneRepositoryAndExtractExports("https://github.com/aws/aws-sdk-go-v2", "service/s3/v1.30.0", "s3", "/service/s3")
 	heliosExports := extractProxyLibExports("helioss3")
-	originalExports = deleteByName(originalExports,"NewDefaultEndpointResolver")
+	originalExports = deleteByName(originalExports,"NewDefaultEndpointResolver") // this method signature was changed, hence deleting it from comparison
 	heliosExports = deleteByName(heliosExports,"NewDefaultEndpointResolver")
 	assert.EqualValues(t, originalExports, heliosExports)
 }
@@ -81,8 +81,8 @@ func TestS3InterfaceMatch(t *testing.T) {
 func TestDynamoDbInterfaceMatch(t *testing.T) {
 	originalExports := cloneRepositoryAndExtractExports("https://github.com/aws/aws-sdk-go-v2", "service/dynamodb/v1.18.0", "dynamodb", "/service/dynamodb")
 	heliosExports := extractProxyLibExports("heliosdynamodb")
-	originalExports = deleteByName(originalExports,"NewDefaultEndpointResolver")
-	heliosExports = deleteByName(heliosExports,"NewDefaultEndpointResolver")
+	originalExports = deleteByName(originalExports,"NewDefaultEndpointResolver") // this method signature was changed, hence deleting it from comparison
+	heliosExports = deleteByName(heliosExports,"NewDefaultEndpointResolver") 
 	assert.EqualValues(t, originalExports, heliosExports)
 }
 
