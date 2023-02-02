7217984f8058c62187997ae63a1d1357089ddf05
Ran Nozik <ran@gethelios.dev>
2023-02-02 08:27:16 +0200

    update instrumentation dependency

--- proxy-libs/helioshttp/http.go
+++ proxy-libs/helioshttp/http.go
@@ -10,7 +10,7 @@ import (
 	"net/url"
 	"time"
 
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"github.com/helios/opentelemetry-go-contrib/instrumentation/net/http/otelhttp"
 )
 
 // Errors used by the HTTP server.
