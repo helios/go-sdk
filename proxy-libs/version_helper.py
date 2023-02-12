0b834cbaa82d3615997bf9a54c7f5538c25410dd
LironKS <liron@gethelios.dev>
2023-02-12 11:11:15 +0200

    update python script

--- proxy-libs/version_helper.py
+++ proxy-libs/version_helper.py
@@ -8,10 +8,15 @@ PROXY_LIBS_PREFIX = 'github.com/helios/go-sdk/proxy-libs'
 def main():
     proxy_lib_name = sys.argv[1]
     proxy_lib_version = sys.argv[2]
-    min_version_supported = sys.argv[3]
+    min_supported_version = sys.argv[3]
     with open(PROXY_LIBS_FILE_NAME) as f:
         proxy_libs_versions = json.load(f)
-        proxy_libs_versions[("%s/%s" % (PROXY_LIBS_PREFIX,proxy_lib_name))][min_version_supported] = proxy_lib_version
+        proxy_lib_full_path = ("%s/%s" % (PROXY_LIBS_PREFIX,proxy_lib_name))
+        min_supported_versions = proxy_libs_versions[proxy_lib_full_path]
+        for version in min_supported_versions:
+            if version["minSupportedVersion"] == min_supported_version:
+                version["proxyLibVersion"] = proxy_lib_version
+
     os.remove(PROXY_LIBS_FILE_NAME)
     with open(PROXY_LIBS_FILE_NAME, 'w') as output:
         json.dump(proxy_libs_versions, output)
