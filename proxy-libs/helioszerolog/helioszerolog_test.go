55d5bb40e57bb31126397a1f5ddf223246cf5327
natashz <nata7che@gmail.com>
2023-03-02 16:45:12 +0200

    added HS_DISABLED to helioszerolog

--- proxy-libs/helioszerolog/helioszerolog_test.go
+++ proxy-libs/helioszerolog/helioszerolog_test.go
@@ -5,6 +5,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
+
 	// "os"
 	"strings"
 	"testing"
@@ -40,12 +42,38 @@ func TestZerolog(t *testing.T) {
 	assert.True(t, ok)
 	traceId := resultedSpan.SpanContext().TraceID().String()
 	spanId := resultedSpan.SpanContext().SpanID().String()
-	testedVal := fmt.Sprintf("https://app.gethelios.dev?actionTraceId=%s&spanId=%s&source=zerolog", traceId, spanId)
-	assert.True(t, strings.HasPrefix(value, testedVal))
+	expectedGoToHeliosVal := fmt.Sprintf("https://app.gethelios.dev?actionTraceId=%s&spanId=%s&source=zerolog", traceId, spanId)
+	assert.True(t, strings.HasPrefix(value, expectedGoToHeliosVal))
 	value, ok = data["traceId"]
 	assert.True(t, ok)
 	assert.Equal(t, traceId, value)
 	value, ok = data["spanId"]
 	assert.True(t, ok)
 	assert.Equal(t, spanId, value)
+}
+
+func TestDisableInstrumentation(t *testing.T) {
+	os.Setenv("HS_DISABLED", "true")
+	defer os.Setenv("HS_DISABLED", "")
+
+	sr := tracetest.NewSpanRecorder()
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSpanProcessor(sr))
+	otel.SetTracerProvider(tp)
+	propagator := propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{})
+	otel.SetTextMapPropagator(propagator)
+	tracer := tp.Tracer("TestZerolog")
+	ctx, span := tracer.Start(context.Background(), "hello-span")
+
+	out := &bytes.Buffer{}
+	newLogger := NewWithContext(out, ctx)
+	newLogger.Info().Msg("test")
+	data := map[string]string{}
+	json.Unmarshal(out.Bytes(), &data)
+
+	span.End()
+	sr.ForceFlush(ctx)
+	spans := sr.Ended()
+	assert.Equal(t, 1, len(spans))
+	_, exists := data["go_to_helios"]
+	assert.False(t, exists)
 }
